50ffd87555bc4a74c9a6c61c404c90e933bd65c7
David Huertas <davidhuertasortiz@gmail.com>
2024-03-13 20:44:20 +0100

    ✨ Add @astrojs/vercel package and configure Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
       applyBaseStyles: false
     }),
     react()
-  ]
+  ],
+  adapter: vercel()
 })
